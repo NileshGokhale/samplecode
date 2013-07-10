7aa651bec9fd12de8b7b609e6436a3aa8ce43961
NileshGokhale <nileshg@xpanxion.co.in>
2013-07-10 17:10:14 +0530

    base controller added to project

---
+++ GuestBook/Controllers/BaseController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GuestBook.Helpers;
+
+namespace GuestBook.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.HttpContext.Request.IsAjaxRequest())
+            {
+                SessionHelper.AjaxRequest = filterContext.ActionDescriptor.ActionName;
+            }
+            base.OnActionExecuting(filterContext);
+        }
+
+        protected void ClearAjaxRequest()
+        {
+            
+        }
+    }
+}
