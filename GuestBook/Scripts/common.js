8955918a9c0b8d6311cad4377ee7faf1a94d7f9a
Nilesh Gokhale <nilesh_s_gokhale@hotmail.com>
2013-07-18 20:42:22 +0530

    add common js file

---
+++ GuestBook/Scripts/common.js
@@ -0,0 +1,9 @@
+﻿function showMenu(show) {
+    if (show) {
+        $("#submenu").removeClass("hiddenSubmenu");
+        $("#submenu").addClass("submenu");
+    } else {
+        $("#submenu").removeClass("submenu");
+        $("#submenu").addClass("hiddenSubmenu");
+    }
+}
